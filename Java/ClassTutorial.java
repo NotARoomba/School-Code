8719fed36d75d53762bafba12c18411d8bb76860
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2021-12-01 14:00:38 -0600

    classes

---
+++ Java/ClassTutorial.java
@@ -0,0 +1,7 @@
+public class ClassTutorial {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Animal penguin = new Animal();
+        System.out.println(penguin.age);
+    }
+}
