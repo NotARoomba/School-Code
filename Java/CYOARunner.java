665f30e317a0dd80a299c4907fe50240df10b4bb
Photosyntheses <kg5inb@gmail.com>
2022-03-02 12:31:29 -0600

    ewtat

---
+++ Java/CYOARunner.java
@@ -0,0 +1,10 @@
+/*
+Nathan Alspaugh
+5th period
+*/
+public class CYOARunner {
+    public static void main(String[] args) {
+      CYOA life = new CYOA();
+      life.print(life.exe());
+    }
+  }
