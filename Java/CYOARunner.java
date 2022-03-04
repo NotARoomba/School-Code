bf2ed7609669ea0f9d6444d8223be7f380775097
Photosyntheses <kg5inb@gmail.com>
2022-03-04 12:22:16 -0600

    aa

--- Java/CYOARunner.java
+++ Java/CYOARunner.java
@@ -2,9 +2,15 @@
 Nathan Alspaugh
 5th period
 */
+import java.util.*;
 public class CYOARunner {
     public static void main(String[] args) {
-      CYOA advenure = new CYOA();
-      advenure.print(advenure.startAdventure());
+      Scanner input = new Scanner(System.in);
+        
+        System.out.println("What is your character name?");
+        String name = input.nextLine();
+        
+      CYOA adventure = new CYOA();
+      adventure.print(adventure.startAdventure(input, name));
     }
   }
