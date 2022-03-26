1fdef6b8f84e514b84ed8629a04c86f79c074dc7
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2022-03-25 20:04:53 -0500

    git

---
+++ Java/Divisors.txt
@@ -0,0 +1,23 @@
+public class Divisors {
+    public static void main(String[] args) {
+        printDivisors(45);
+        System.out.println();
+        printDivisors(45);
+    }
+    public static void printDivisors(int number) {
+        int divisor = 1;
+        while (divisor <= number) {
+            if (number % divisor == 0) {
+                System.out.print(divisor + " ");
+            }
+            divisor++;
+        }
+    }
+    public static void printDivisorsForLoop(int number) {
+        for (int divisor = 1; divisor <= number; divisor++) {
+            if (number % divisor == 0) {
+                System.out.print(divisor + " ");
+            }
+        }
+    }
+}
--- Java/test.java
+++ Java/test.java
@@ -4,15 +4,14 @@ import java.io.IOException;
 import java.util.*;
 import java.util.ArrayList;
 public class test {
-    public static void main(String[] args) throws IOException {
-        List<Integer> indexes = new ArrayList<Integer>();
-            String text = new BufferedReader(new FileReader("C:\\Windows\\explorer.exe")).lines().toString();
-            for(int i = 0; i < 26; i++){
-                indexes.add(text.indexOf((char)(97 + i)));
-            }
-            for(int i = 0; i < 10; i++){
-                indexes.add(text.indexOf((char)(48 + i)));
-            }
-     }
-     //make a discord bot
+public static void main(String[] args) throws IOException {
+    List<Integer> indexes = new ArrayList<Integer>();
+        String text = new BufferedReader(new FileReader("C:\\Windows\\explorer.exe")).lines().toString();
+        for(int i = 0; i < 26; i++){
+            indexes.add(text.indexOf((char)(97 + i)));
+        }
+        for(int i = 0; i < 10; i++){
+            indexes.add(text.indexOf((char)(48 + i)));
+        }
+    }
 }
