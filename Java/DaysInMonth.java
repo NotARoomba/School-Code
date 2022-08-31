20512df071e48adb6132418a611cb8843cca89c3
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2022-08-30 19:04:28 -0500

    Update DaysInMonth.java

--- Java/DaysInMonth.java
+++ Java/DaysInMonth.java
@@ -2,26 +2,14 @@ import java.util.*;
 
 
 public class DaysInMonth {
-    static int b;
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         System.out.print("Enter a month number: ");
         int b = a.nextInt();
-        boolean g = i();
+        System.out.print("Enter a year: ");
+        int c = a.nextInt();
+        boolean g = c%4==0&&(c%100!=0||c%400==0)?true:false;
         System.out.println(String.format("Days in month: %d\nLeap Year: %s", b<8?b%2==1?31:b==2?g?29:28:30:b%2==1?30:31, g));
 
-    }
-    public static boolean i() {
-        System.out.print("Enter a year: ");
-        Scanner r = new Scanner(System.in);
-        return c(r, r.nextInt());
-    }
-
-    public static boolean c(Scanner f, int h) {
-        return (b = ((int) ((h / ((int) 'd' + 'b') - ('c' * 99) + (65 * (int) 'A')) * -1)
-                + ((h / ((int) (14.071 * 14.071) + 1) - (9801)
-                + (((int) (4225 / 32.5) / 2)) * (((int) 'a') - ((int) '@' / 2)))
-                * 1)
-                + h)) % 4 == 0 && (b % 100 != 0 || b % 400 == 0) ? true : false;
     }
 }
