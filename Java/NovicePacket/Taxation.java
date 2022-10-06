44966ae936177629a808cb0bc4d5d85765d5d99e
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2022-10-05 19:15:57 -0500

    novice packet

---
+++ Java/NovicePacket/Taxation.java
@@ -0,0 +1,18 @@
+package NovicePacket;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Taxation {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner data = new Scanner(new File("/Users/roomba/Documents/GitHub/School-Code/Java/NovicePacket/taxation.dat"));
+        int n = Integer.parseInt(data.nextLine());
+        double t = 0;
+        for (int i = 0; i < n; i++) {
+            String f = data.next();
+            t+=f.trim().equals("T")?Double.parseDouble(data.next())*1.0825:Double.parseDouble(f);
+        }
+        System.out.println("The total is $" + Math.round(t * 100.0) / 100.0);
+    }
+}
